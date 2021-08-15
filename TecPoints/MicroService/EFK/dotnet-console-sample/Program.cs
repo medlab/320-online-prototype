da144ee3e6193d0d8d67fb809d6fec067fd3d85d
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2021-08-15 21:01:22 +0800

    add dotnet console sample with serilog redirect to console

---
+++ TecPoints/MicroService/EFK/dotnet-console-sample/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ref: https://github.com/serilog/serilog/wiki/Configuration-Basics
+// ref: https://github.com/serilog/serilog/wiki/Enrichment
+
+using System;
+using System.Threading.Tasks;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+ .Enrich.WithThreadId()
+ .WriteTo.Console()
+    .CreateLogger();
+
+Log.Information("--begin--");
+
+int counter=1;
+do{
+    Log.Information("counter: {counter}", counter);
+    await Task.Delay(TimeSpan.FromSeconds(1));
+    counter++;
+
+}while(true);
+
+
+Log.Information("--end--");
+
+Log.CloseAndFlush();
